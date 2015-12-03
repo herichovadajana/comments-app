08266cebdf89ae917c513f51770b5a99311d028f
DajanaStiberova <dajana.stiberova@gmail.com>
2015-12-03 19:42:18 +0100

    implement mutations

--- src/om_comment_box/core.cljs
+++ src/om_comment_box/core.cljs
@@ -18,29 +18,40 @@
 
 (defui CommentForm
   Object
+  (initLocalState [this]
+                  {:name "" :message ""})
   (render [this]
-          (dom/div #js {}
-                   (dom/div #js {:className "input-group my-input"}
-                            (dom/span #js {:className "input-group-addon"
-                                           :id "basic-addon"} "Name")
-                            (dom/input #js {:type "text"
-                                            :className "form-controll"
-                                            :id "input-style"
-                                            :placeholder ""}))
-                   (dom/div #js {:className "input-group my-input"}
-                            (dom/span #js {:className "input-group-addon"
-                                           :id "basic-addon"} "Message")
-                            (dom/input #js {:type "text"
-                                            :className "form-controll"
-                                            :id "input-style"
-                                            :placeholder ""}))
-                   (dom/button #js {:type "button"
-                                    :className "btn btn-default btn-lg"}
-                               "send"))))
+          (dom/form #js {:onSubmit (fn [event]
+                                     (.preventDefault event)
+                                     (.log js/console ((:submit-comment (om/props this))
+                                                       {:name (:name (om/get-state this)) :message (:message (om/get-state this))})))}
+                    (dom/div #js {:className "input-group my-input"}
+                             (dom/span #js {:className "input-group-addon"
+                                            :id "basic-addon"} "Name")
+                             (dom/input #js {:type "text"
+                                             :className "form-controll"
+                                             :id "input-style"
+                                             :value (:name (om/get-state this))
+                                             :onChange (fn [event]
+                                                         (om/update-state! this assoc :name (.. event -target -value)))
+                                             :placeholder ""}))
+                    (dom/div #js {:className "input-group my-input"}
+                             (dom/span #js {:className "input-group-addon"
+                                            :id "basic-addon"} "Message")
+                             (dom/input #js {:type "text"
+                                             :className "form-controll"
+                                             :id "input-style"
+                                             :placeholder ""
+                                             :value (:message (om/get-state this))
+                                             :onChange (fn [event]
+                                                         (om/update-state! this assoc :message (.. event -target -value)))}))
+                    (dom/button #js {:type "submit"
+                                     :className "btn btn-default btn-lg"}
+                                "send"))))
 
-(def commentform (om/factory CommentForm))
+(def comment-form (om/factory CommentForm))
 
-(defui Comment
+(defui CommentView
   static om/IQuery
   (query [this]
          [:name :message])
@@ -52,7 +63,7 @@
                     (dom/h3 #js {} " Message: ")
                     (dom/p #js {} message)))))
 
-(def comment (om/factory Comment))
+(def comment-view (om/factory CommentView))
 
 (defui RootView
   static om/IQuery
@@ -63,14 +74,22 @@
           (let [{:keys [comments]} (om/props this)]
             (dom/div #js {:className "coll-md-12 need-margin"}
                      (title)
-                     (commentform)
+                     (comment-form {:comments comments
+                                    :submit-comment (fn [new-comment]
+                                                      (om/transact! this `[(comment/update-input! {:new-comment ~new-comment})]))})
                      (dom/ul #js {:className "comments-list"}
-                             (map comment comments))))))
+                             (map comment-view comments))))))
+
+(defmulti mutate (fn [_ k _] k))
+
+(defmethod mutate 'comment/update-input!
+  [{:keys [state]} _ {:keys [new-comment]}]
+  {:action (fn [] (swap! state update-in [:comments] conj new-comment))})
 
 (defn read [{:keys [state]} k _]
   {:value (get @state k)})
 
-(def parser (om/parser {:read read}))
+(def parser (om/parser {:read read :mutate mutate}))
 
 (def reconciler
   (om/reconciler
